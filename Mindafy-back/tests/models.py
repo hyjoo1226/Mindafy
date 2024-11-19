54352df14af128a36c1f381e7a26dd727508e71a
hyeonho.joo <sparrow9576@gmail.com>
2024-11-19 20:05:42 +0900

    Feat: 전체 테스트 조회, 단일 테스트 조회 API GET 요청

--- Mindafy-back/tests/models.py
+++ Mindafy-back/tests/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    participant_count = models.IntegerField(default=0)
+    duration = models.IntegerField()
+    test_img = models.CharField(max_length=255, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recommendation_count = models.IntegerField(default=0)
+    result = models.CharField(max_length=255)
