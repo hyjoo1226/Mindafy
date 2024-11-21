7cca5e5e73e19768e85ba232b943787345547024
hyjoo <sparrow9576@gmail.com>
2024-11-21 13:03:31 +0900

    Fix: account url 경로 분리

--- Mindafy-back/accounts/urls.py
+++ Mindafy-back/accounts/urls.py
@@ -1,6 +1,10 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from dj_rest_auth import views as dj_rest_auth_views
+from . import views
 
-# urlpatterns = [
-#     path('signup/', views.signup),
-# ]
+urlpatterns = [
+    path('signup/', views.CustomRegisterView.as_view()),
+    path('login/', views.CustomLoginView.as_view()),
+
+    path('password/change/', dj_rest_auth_views.PasswordChangeView.as_view()),
+]
