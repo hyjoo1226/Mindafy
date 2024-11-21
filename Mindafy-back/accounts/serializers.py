e68f5153e53a7c1f083540cac626d6b34a558539
hyjoo <sparrow9576@gmail.com>
2024-11-21 11:22:14 +0900

    Fix: 로그인 API 정상응답값 수정

--- Mindafy-back/accounts/serializers.py
+++ Mindafy-back/accounts/serializers.py
@@ -1,5 +1,10 @@
+from rest_framework.response import Response
+
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import LoginSerializer
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import authenticate
 from .models import User
 
 class CustomRegisterSerializer(RegisterSerializer):
@@ -8,6 +13,12 @@ class CustomRegisterSerializer(RegisterSerializer):
     email = serializers.EmailField(required=True)
     profile_img = serializers.CharField(required=False, allow_null=True)
     
+
+    def validate_age(self, age):
+        if age <= 0 or age > 200:
+             raise serializers.ValidationError("나이를 올바르게 입력해주세요.")
+        return age
+
     def save(self, request):
         user = super().save(request)
         user.nickname = self.validated_data['nickname']
@@ -17,4 +28,27 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.save()
 
         return user
-    
+
+# class CustomLoginSerializer(LoginSerializer):
+#     def validate(self, attrs):
+#         user = authenticate(
+#             username=attrs.get('username'),
+#             password=attrs.get('password')
+#         )
+
+#         if not user:
+#             raise ValidationError("잘못된 자격 증명입니다.")
+
+#         data = super().validate(attrs)
+
+#         data['user'] = {
+#                 "id": user.id,
+#                 "username": user.username,
+#                 "email": user.email,
+#                 "nickname": user.nickname,
+#                 "profile_img": user.profile_img
+#             }
+
+#         return data
+class CustomLoginSerializer(LoginSerializer):
+    pass
--- Mindafy-back/accounts/views.py
+++ Mindafy-back/accounts/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework import status
 
+from dj_rest_auth.views import LoginView
 from dj_rest_auth.registration.views import RegisterView
-from .serializers import CustomRegisterSerializer
+from .serializers import CustomRegisterSerializer, CustomLoginSerializer
 from .models import User
 
 class CustomRegisterView(RegisterView):
@@ -24,9 +25,33 @@ class CustomRegisterView(RegisterView):
             }
             user.save()
             return Response({
-                # "user": CustomRegisterSerializer(user).data,
                 "user": user_data,
                 "key": response.data['key']
             }, status=status.HTTP_201_CREATED)
+
+        return response
+
+class CustomLoginView(LoginView):
+    serializer_class = CustomLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            user = User.objects.get(username=request.data['username'])
+            user_data = {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "nickname": user.nickname,
+                "age": user.age,
+                "profile_img": user.profile_img,
+                "created_at": user.created_at
+            }
+            user.save()
+            return Response({
+                "user": user_data,
+                "key": response.data['key']
+            }, status=status.HTTP_200_OK)
 
         return response
