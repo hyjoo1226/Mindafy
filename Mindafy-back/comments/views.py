c6dd9beb298a23f8336fd2c72a55515f7c96b0e7
hyjoo <sparrow9576@gmail.com>
2024-11-22 13:18:03 +0900

    Fix: 댓글 API 엔드포인트 RESTful하게 수정

--- Mindafy-back/comments/views.py
+++ Mindafy-back/comments/views.py
@@ -11,14 +11,31 @@ from .models import Comment, Test
 from .serializers import CommentSerializer
 from django.shortcuts import get_list_or_404, get_object_or_404
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def comments(request, test_id=None):
     if test_id:
         test = get_object_or_404(Test, id=test_id)
-        comments = test.comments.all()
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # 해당 게시글의 전체 댓글 조회
+        if request.method == 'GET':
+            comments = test.comments.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        # 해당 게시글에 댓글 생성
+        elif request.method == 'POST':
+            if not request.user.is_authenticated:
+                return Response({'error': '로그인이 필요합니다.'}, status=status.HTTP_401_UNAUTHORIZED)
+            comment_data = {
+                'user': request.user.id,
+                'test': test_id,
+                'content': request.data.get('content'),
+                'parent_comment': request.data.get('parent_comment')
+            }
+            serializer = CommentSerializer(data=comment_data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #전체 댓글 조회
     else:
         comments = get_list_or_404(Comment)
         serializer = CommentSerializer(comments, many=True)
@@ -27,28 +44,14 @@ def comments(request, test_id=None):
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def comment_detail(request, comment_id, test_id=None):
+    # 해당 게시글의 단일 댓글 조회
     if test_id:
         test = get_object_or_404(Test, id=test_id)
         comment = get_object_or_404(test.comments, id=comment_id)
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    # 해당 게시글의 전체 댓글 조회
     else:
         comment = get_object_or_404(Comment, id=comment_id)
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-def create_comment(request, test_id):
-    comment_data = {
-        'user': request.user.id,
-        'test': test_id,
-        'content': request.data.get('content'),
-        'parent_comment': request.data.get('parent_comment')
-    }
-    
-    serializer = CommentSerializer(data=comment_data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
