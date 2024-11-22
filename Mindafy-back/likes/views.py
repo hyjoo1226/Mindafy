2db4d306237724586ce0766c39c3d61d09174796
hyjoo <sparrow9576@gmail.com>
2024-11-22 15:49:25 +0900

    Feat: 테스트 좋아요 취소 API

--- Mindafy-back/likes/views.py
+++ Mindafy-back/likes/views.py
@@ -10,10 +10,17 @@ from .models import Test, Comment, Like
 @api_view(['POST', 'DELETE'])
 def test_like(request, test_id):
     test = get_object_or_404(Test, id=test_id)
+    like = Like.objects.filter(user=request.user, test_id=test_id)
     if request.method == 'POST':
-        like_exist = Like.objects.filter(user=request.user, test_id=test_id)
-        if not like_exist:
+        if not like:
             Like.objects.create(user=request.user, test_id=test_id)
             Test.objects.filter(id=test_id).update(recommendation_count=F('recommendation_count') + 1)
             return Response(status=status.HTTP_201_CREATED)
         return Response({'error': '이미 좋아요를 눌렀습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        if like:
+            like.delete()
+            Test.objects.filter(id=test_id).update(recommendation_count=F('recommendation_count') - 1)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': '좋아요를 누르지 않았습니다.'}, status=status.HTTP_400_BAD_REQUEST)
