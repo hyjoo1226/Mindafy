00af5d8e120159ac362ad792e14888e2dd7be300
박태현 <rinma772@naver.com>
2024-11-22 17:52:40 +0900

    Feat: create-comment

--- Mindafy-front/front/src/components/CommentList.vue
+++ Mindafy-front/front/src/components/CommentList.vue
@@ -1,7 +1,7 @@
 <!-- CommentList.vue -->
 <template>
     <div>
-        <h3>댓글</h3>
+        <h3>댓글 목록</h3>
         <div v-if="comments.length > 0">
             <CommentListItem 
                 v-for="comment in comments" 
@@ -32,14 +32,20 @@ const fetchComments = () => {
         url: `${store.API_URL}/api/v1/tests/${route.params.id}/comments/`,
     })
         .then((res) => {
-        comments.value = res.data;
+            comments.value = res.data;
         })
         .catch((err) => console.log(err));
 };
 
 // 컴포넌트가 마운트되면 댓글을 가져옵니다.
 onMounted(() => {
-    fetchComments();
+    fetchComments(); // 컴포넌트 마운트 시 댓글 가져오기
+});
+
+
+// fetchComments 메서드를 부모에서 호출 가능하게 공개
+defineExpose({
+  fetchComments,
 });
 </script>
 
