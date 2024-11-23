b33e7ced78d64e366e9f5ec7838c55417e0329c2
hyeonho.joo <sparrow9576@gmail.com>
2024-11-23 23:11:05 +0900

    Update: 좋아요 로직 변경

--- Mindafy-back/likes/models.py
+++ Mindafy-back/likes/models.py
@@ -7,4 +7,5 @@ class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     test = models.ForeignKey(Test, on_delete=models.CASCADE, null=True, related_name='test_like')
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, related_name='comment_like')
+    is_like = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
