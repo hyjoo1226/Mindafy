83f33735ce65f65089def8aec412f86ceba14971
hyeonho.joo <sparrow9576@gmail.com>
2024-11-24 17:40:12 +0900

    Feat: 예금 적금 데이터 초기 저장 API

---
+++ Mindafy-back/finance/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class DepositProducts(models.Model):
+        fin_prdt_cd = models.TextField()
+        kor_co_nm = models.TextField()
+        fin_prdt_nm = models.TextField()
+        mtrt_int = models.TextField()
+        spcl_cnd = models.TextField()
+        etc_note = models.TextField()
+
+class DepositOptions(models.Model):
+    product = models.ForeignKey(DepositProducts, on_delete=models.CASCADE)
+    intr_rate = models.FloatField()
+    intr_rate2 = models.FloatField()
+    save_trm = models.IntegerField()
+
+class SavingProducts(models.Model):
+        fin_prdt_cd = models.TextField()
+        kor_co_nm = models.TextField()
+        fin_prdt_nm = models.TextField()
+        mtrt_int = models.TextField()
+        spcl_cnd = models.TextField()
+        etc_note = models.TextField()
