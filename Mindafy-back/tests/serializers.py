e5a56a1420452b444069b395c069902faa3e5d2e
hyeonho.joo <sparrow9576@gmail.com>
2024-11-26 23:44:09 +0900

    Feat: 샘플데이터 완성

--- Mindafy-back/tests/serializers.py
+++ Mindafy-back/tests/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.conf import settings
 from .models import Test, TestResult
 
 
@@ -6,6 +7,13 @@ class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
         fields = '__all__'
+    
+    def get_test_img(self, obj):
+        if obj.test_img:
+            request = self.context.get('request')
+            img_path = obj.test_img
+            return request.build_absolute_uri(settings.STATIC_URL + img_path)
+        return None
 
 class TestResultSerializer(serializers.ModelSerializer):
     class Meta:
